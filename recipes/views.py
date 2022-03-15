eb1693a998352f0780f2ef7f93a96f331dbc4e69
Luiz Honorato <lhehonorato@gmail.com>
2022-03-15 14:24:51 -0300

    Finishing recipes page.

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
-
-
-def contato(request):
-    return HttpResponse('Contato')
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Luiz Honorato'
+    })
