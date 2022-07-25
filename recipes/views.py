e2b4070156bcd907730f5f208d5962c9f545b15d
Luiz Honorato <lhehonorato@gmail.com>
2022-07-25 11:52:01 -0300

    save changes

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 # Create your views here.
 
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Luiz Honorato'
+        'recipes': [make_recipe() for _ in range(10)]
     })
 
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Luiz Honorato'
+        'recipe': make_recipe(),
+        'is_detail_page': True
     })
